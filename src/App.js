015a5270bdaad2250fda5e9764ba1a1448e83aca
Kian-F <52603628+Kian-F@users.noreply.github.com>
2020-11-24 23:08:37 +1100

    pages

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
-import React from 'react';
+import React from "react";
 import Nav from "./Nav";
-import Shop from "./Shop"
-import About from "./About"
+import Shop from "./Shop";
+import About from "./About";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-      <Nav/>
-      <Shop/>
-      <About/>
-
-    </div>
+    
+      <Router>
+        <div>
+        <Nav />
+        <Switch>
+        <Route path='/home' exact component={Home}/>
+        <Route path="/shop" component={Shop} />
+        <Route path='/About' component={About}/>
+        </Switch>
+        </div>
+      </Router>
+      
+    
   );
 }
 
+const Home=()=>{
+return(
+  <h3>Home</h3>
+)
+}
 export default App;
