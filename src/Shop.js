35b4db56a345d5654f1f7f5c63bbf4bb0694c732
Kian-F <52603628+Kian-F@users.noreply.github.com>
2020-12-03 15:41:12 +1100

    materila ui

--- src/Shop.js
+++ src/Shop.js
@@ -1,7 +1,25 @@
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles({
+  root:{
+    maxWidth: 345,
+  },
+  media:{
+    height: 140,
+  },
+})
 
 function Shop() {
+  const classes = useStyles();
   const [products, setProducts] = useState([]);
   useEffect(() => {
     fetchProducts();
@@ -18,9 +36,9 @@ function Shop() {
       <div className="products">
         {products.map((product) => (
           <ul >
-            <h3 key={product.id}>
+            <li><h3 key={product.id}>
               <Link to={`/shop/${product.id}`}>{product.title}</Link>
-            </h3>
+            </h3></li>
             <h4>{product.category}</h4>
             <img src={product.image}></img>
           </ul>
