fe79a58aeae8a5fa0658ccfc14ca684bacb8a4ee
Kian-F <52603628+Kian-F@users.noreply.github.com>
2020-12-04 15:40:44 +1100

    materila ui

--- src/Shop.js
+++ src/Shop.js
@@ -1,22 +1,25 @@
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+import Grid from "@material-ui/core/Grid"
 
 const useStyles = makeStyles({
-  root:{
+  root: {
+    flexGrow: 1,
     maxWidth: 345,
   },
-  media:{
-    height: 140,
+  media: {
+    width: 400,
+    height: 400,
   },
-})
+});
 
 function Shop() {
   const classes = useStyles();
@@ -33,17 +36,24 @@ function Shop() {
   return (
     <div>
       <h3>Shop</h3>
-      <div className="products">
+      <Grid container className={classes.root.flexGrow} spacing={2}>
+        <Grid item xs={3}>
+      <card className={classes.root}>
         {products.map((product) => (
-          <ul >
-            <li><h3 key={product.id}>
-              <Link to={`/shop/${product.id}`}>{product.title}</Link>
-            </h3></li>
-            <h4>{product.category}</h4>
-            <img src={product.image}></img>
-          </ul>
+          <CardActionArea>
+            <CardMedia className={classes.media} image={product.image} />
+            <CardContent>
+              <Typography key={product.id} gutterBottom variant='h5' component='h2'>
+                <Link to={`/shop/${product.id}`}>{product.title}</Link>
+              </Typography>
+            
+            <Typography>{product.category}</Typography>
+            </CardContent>
+          </CardActionArea>
         ))}
-      </div>
+      </card>
+      </Grid>
+      </Grid>
     </div>
   );
 }
